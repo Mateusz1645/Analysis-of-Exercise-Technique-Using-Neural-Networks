2595e3cd6d8f69d439cd73960d277d66edb70d3f
Mateusz Skrzątek <skrzatekmateusz@gmail.com>
2025-09-19 17:44:20 +0200

    FIRST MODEL

--- codes/csv_generate.py
+++ codes/csv_generate.py
@@ -16,6 +16,8 @@ base_dir = os.getenv("BASE_DIR")
 output_csv = os.getenv("OUTPUT_CSV")
 log_file = os.getenv("WARNING_TXT")
 target_len = 90
+random_add = 1  # liczba wariantów
+max_shift = 10  # maksymalne przesunięcie w pikselach
 show_preview = False
 
 # === LANDMARKI ===
@@ -169,11 +171,30 @@ def process_video(video_file, label, video_id):
     resampled_mirror = resampled.copy()
     resampled_mirror[:, 0::2] = frame_width - resampled_mirror[:, 0::2]
     for i in range(target_len):
-        row = {'video_id': video_id + 1, 'frame': i, 'label': label}
+        if label == 5:
+            row = {'video_id': video_id + 1, 'frame': i, 'label': 6}
+        elif label == 6:
+            row = {'video_id': video_id + 1, 'frame': i, 'label': 5}
+        else:
+            row = {'video_id': video_id + 1, 'frame': i, 'label': label}
         for j, idx in enumerate(SELECTED_LANDMARKS):
             row[f"{LANDMARKS_INFO[idx]}_x"] = resampled_mirror[i, j*2]
             row[f"{LANDMARKS_INFO[idx]}_y"] = resampled_mirror[i, j*2+1]
         rows.append(row)
+
+    for k in range(random_add):
+        dx = np.random.uniform(-max_shift, max_shift)
+        dy = np.random.uniform(-max_shift, max_shift)
+        augmented = resampled.copy()
+        augmented[:, 0::2] += dx  # x
+        augmented[:, 1::2] += dy  # y
+
+        for i in range(target_len):
+            row = {'video_id': video_id + 2 + k, 'frame': i, 'label': label}
+            for j, idx in enumerate(SELECTED_LANDMARKS):
+                row[f"{LANDMARKS_INFO[idx]}_x"] = augmented[i, j*2]
+                row[f"{LANDMARKS_INFO[idx]}_y"] = augmented[i, j*2+1]
+            rows.append(row)
 
     return rows
 
@@ -195,7 +216,7 @@ if __name__ == "__main__":
             continue
         for video_file in glob(os.path.join(label_path, "*.mp4")):
             video_list.append((video_file, label, video_id))
-            video_id += 2  # normal + mirror
+            video_id += 2 + random_add  # normal + mirror + augmentacja
 
     video_count = len(video_list)
     print(f"Znaleziono {video_count} wideo do przetworzenia.")
