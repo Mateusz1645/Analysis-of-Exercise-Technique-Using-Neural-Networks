73953b11876d9adff605dc3b98aea36606f35e03
Mateusz Skrzątek <skrzatekmateusz@gmail.com>
2025-10-03 11:12:19 +0200

    fix mirroring problem with labelin df

--- data_processing/config.py
+++ data_processing/config.py
@@ -25,5 +25,16 @@ LANDMARKS_INFO = {
 }
 SELECTED_LANDMARKS = list(LANDMARKS_INFO.keys())
 
+LANDMARKS_INFO_MIRRORED = {
+    0: "nose",
+    12: "left_shoulder", 11: "right_shoulder",
+    14: "left_elbow", 13: "right_elbow",
+    16: "left_wrist", 15: "right_wrist",
+    24: "left_hip", 23: "right_hip",
+    26: "left_knee", 25: "right_knee",
+    28: "left_ankle", 27: "right_ankle"
+}
+SELECTED_LANDMARKS_MIRRORED = list(LANDMARKS_INFO_MIRRORED.keys())
+
 # === WARNING CHECK PARAMETERS ===
 CHECK_FRAMES = 5  # number of first frames to check for abnormal positions
